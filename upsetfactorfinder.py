72482052a913d62403a41c94e9342d032a2ad270
the-newdeal <alex@hagdav.com>
2022-07-13 02:59:14 -0500

    if name -- main mode

--- upsetfactorfinder.py
+++ upsetfactorfinder.py
@@ -39,51 +39,52 @@ def seedplacing(topx):
     
     return sp
 
-f = open("seeding.csv")      
-seedlist = f.readlines()
-f.close()
-seeddictionary = {}
-for seeding in seedlist:
-    s = seeding.split(",")
-    seeddictionary.update({s[11].strip('"'):int(s[4])}) 
+if __name__ == "main":
+    f = open("seeding.csv")      
+    seedlist = f.readlines()
+    f.close()
+    seeddictionary = {}
+    for seeding in seedlist:
+        s = seeding.split(",")
+        seeddictionary.update({s[11].strip('"'):int(s[4])}) 
 
-print(seeddictionary)
+    print(seeddictionary)
 
-tournamentname = "Get Smashed at Tin Roof"
+    tournamentname = "Get Smashed at Tin Roof"
 
-while(True):
-    try:
-        winner = input("gamertag of winning player:\n")
-        loser = input("gamertag of losing player:\n")
-        winningseed = seeddictionary[winner]
-        print("found winning gamer!")
-        losingseed = seeddictionary[loser]
-        print("found losing gamer")
-        
-        print("surprise margin finder:")
-        print("winning seed: {}".format(winningseed))
-        print("lower seed: {}".format(losingseed))
-        if (winningseed > losingseed):
-            LowerTopXthseed = topX(placement(winningseed))
-            HigherTopXthseed = topX(placement(losingseed))
+    while(True):
+        try:
+            winner = input("gamertag of winning player:\n")
+            loser = input("gamertag of losing player:\n")
+            winningseed = seeddictionary[winner]
+            print("found winning gamer!")
+            losingseed = seeddictionary[loser]
+            print("found losing gamer")
+            
+            print("surprise margin finder:")
+            print("winning seed: {}".format(winningseed))
+            print("lower seed: {}".format(losingseed))
+            if (winningseed > losingseed):
+                LowerTopXthseed = topX(placement(winningseed))
+                HigherTopXthseed = topX(placement(losingseed))
 
-            surprisemargin = seedplacing(LowerTopXthseed) - seedplacing(HigherTopXthseed)
-            if (surprisemargin > 0):
-                bracketside = int(input("W(inners) or L(osers):\n"))
-                bracketside = "🔵W" if bracketside else "🔴L"
-                setcount = input("Set count? :\n")
-                SM = "surprise margin of : {}".format(surprisemargin)
-                tweet = "{} {} {} {}, {} @ {}".format(bracketside,winner,setcount,loser,SM,tournamentname)
-                print (tweet)
-                writer = open("upsetsTR.txt", "a", encoding="utf-8")
-                writer.write(tweet)
-                writer.write("\n")
-                writer.close()
+                surprisemargin = seedplacing(LowerTopXthseed) - seedplacing(HigherTopXthseed)
+                if (surprisemargin > 0):
+                    bracketside = int(input("W(inners) or L(osers):\n"))
+                    bracketside = "🔵W" if bracketside else "🔴L"
+                    setcount = input("Set count? :\n")
+                    SM = "surprise margin of : {}".format(surprisemargin)
+                    tweet = "{} {} {} {}, {} @ {}".format(bracketside,winner,setcount,loser,SM,tournamentname)
+                    print (tweet)
+                    writer = open("upsetsTR.txt", "a", encoding="utf-8")
+                    writer.write(tweet)
+                    writer.write("\n")
+                    writer.close()
+                else:
+                    print("upset factor 0")
             else:
-                print("upset factor 0")
-        else:
-            print("no upset")
-    except KeyError:
-        print("one or more gamertags not found! please try again")
-    
+                print("no upset")
+        except KeyError:
+            print("one or more gamertags not found! please try again")
+        
 
